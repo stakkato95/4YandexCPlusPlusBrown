018fad5db7f445d53c604a887c966dfb3cc1450a
Artsiom Kaliaha <stakkato95@gmail.com>
2020-04-05 13:29:45 +0200

    Add example with as_const

---
+++ brown_playground/brown_playground/23_const_hack.cpp
@@ -0,0 +1,26 @@
+//
+//  23_const_hack.cpp
+//  brown_playground
+//
+//  Created by Artsiom Kaliaha on 4/5/20.
+//  Copyright © 2020 Artsiom Kaliaha. All rights reserved.
+//
+
+#include <iostream>
+
+#include <string>
+
+#include <utility>
+#include <cassert>
+
+using namespace std;
+
+int main() {
+    string str = "hello world";
+    //ref to a const string
+    const string& strRef = as_const(str);
+    
+    cout << strRef << endl;
+    
+    return 0;
+}
