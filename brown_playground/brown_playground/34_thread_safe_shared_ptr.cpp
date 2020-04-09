f594607150dcb5c6d977f6cc0da53268a78fed66
Artsiom Kaliaha <stakkato95@gmail.com>
2020-04-09 09:48:17 +0200

    Add example with thread safe shared_ptr

---
+++ brown_playground/brown_playground/34_thread_safe_shared_ptr.cpp
@@ -0,0 +1,30 @@
+//
+//  34_thread_safe_shared_ptr.cpp
+//  brown_playground
+//
+//  Created by Artsiom Kaliaha on 4/9/20.
+//  Copyright © 2020 Artsiom Kaliaha. All rights reserved.
+//
+
+#include <iostream>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+struct Data {
+    string data;
+    
+    Data(string s) : data { s } { }
+};
+
+int main() {
+    //shared_ptr is by default thread safe (incremeanting reference count)
+    //shared_ptr can have a pointer to a const object. good for concurrency
+    
+    shared_ptr<const Data> ptr = make_shared<const Data>("const string");
+    cout << ptr->data << endl;
+    //ptr->data.push_back('a');//compile error
+    
+    return 0;
+}
