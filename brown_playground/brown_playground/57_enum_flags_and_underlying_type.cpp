2c1c1146c9516cb77c152b62f4af0c6a9673b886
Artsiom Kaliaha <stakkato95@gmail.com>
2020-04-17 08:03:54 +0200

    Add example with underlying type

--- brown_playground/brown_playground/57_enum_flags_and_underlying_type.cpp
+++ brown_playground/brown_playground/57_enum_flags_and_underlying_type.cpp
@@ -6,4 +6,63 @@
 //  Copyright © 2020 Artsiom Kaliaha. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+enum class BitFlag {
+    VISIBLE   = 1 << 0, //001
+    INVISIBLE = 1 << 1, //010
+    GONE      = 1 << 2  //100
+};
+
+BitFlag operator|(BitFlag l, BitFlag r) {
+    return static_cast<BitFlag>(static_cast<int>(l) | static_cast<int>(r));
+}
+
+void operator|=(BitFlag& l, BitFlag r) {
+    l = r | l;
+}
+
+BitFlag operator&(BitFlag l, BitFlag r) {
+    return static_cast<BitFlag>(static_cast<int>(l) & static_cast<int>(r));
+}
+
+ostream& operator<<(ostream& os, const BitFlag& flag) {
+    bool anyValue = false;
+    
+    if ((flag & BitFlag::VISIBLE) == BitFlag::VISIBLE) {
+        anyValue = true;
+        os << "VISIBLE";
+    }
+    if ((flag & BitFlag::INVISIBLE) == BitFlag::INVISIBLE) {
+        anyValue = true;
+        os << (anyValue ? " | " : "") << "INVISIBLE";
+    }
+    if ((flag & BitFlag::GONE) == BitFlag::GONE) {
+        anyValue = true;
+        os << (anyValue ? " | " : "") << "GONE";
+    }
+    
+    return os;
+}
+
+int main() {
+    BitFlag flag = BitFlag::GONE;
+    flag |= BitFlag::VISIBLE;
+    cout << flag << endl;
+    
+    if ((flag & BitFlag::GONE) == BitFlag::GONE) {
+        cout << "GONE is set" << endl;
+    }
+    if ((flag & BitFlag::INVISIBLE) != BitFlag::INVISIBLE) {
+        cout << "INVISIBLE is not set" << endl;
+    }
+    
+    //check underlying type of 'BitFlag'
+    bool same = is_same<int, typename underlying_type<BitFlag>::type>::value;
+    cout << boolalpha;
+    cout << "BitFlag is same as int = " << same << endl;
+    
+    return 0;
+}
